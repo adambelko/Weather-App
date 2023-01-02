19b679f4f70f259b7b2fb4608a60cdfe2382b00c
Adzi <belkoadam@gmail.com>
2023-01-03 07:20:01 +1100

    getting weather data as an object based on location working

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,1 +1,25 @@
-const hey= 10
+const callGeoCoord = async (location) => {
+    try {
+        const response = await fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=27424b54a6ab420d52712155ee6a6ff1`,
+            { mode: "cors" }
+        );
+        const responseData = await response.json();
+        const lat = responseData[0].lat;
+        const lon = responseData[0].lon;
+        callWeatherData(lat, lon);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const callWeatherData = async (lat, lon) => {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=27424b54a6ab420d52712155ee6a6ff1`,
+        { mode: "cors" }
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+};
+
+callGeoCoord("Sydney");
